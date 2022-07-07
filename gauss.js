f69a75cb56c8b2883b6a97cd0d2bb5e81f3a9325
ixyzxyz <108864101+ixyzxyz@users.noreply.github.com>
2022-07-07 11:47:02 +0000

    Add files via upload

---
+++ gauss.js
@@ -0,0 +1,53 @@
+﻿/*
+$ node gauss.js
+*/
+
+"use strict";
+const performance = require("perf_hooks").performance;
+
+class Gauss {
+  constructor(n) {
+    this.a = Array(n).fill().map(() => new Float64Array(n).fill(0));
+    this.b = new Float64Array(n).fill(0);
+  }
+  set(a, b) {
+    const n = this.a.length;
+    for (let i = 0; i < n; i++) {
+      for (let j = i; j < n; j++) {
+        this.a[i][j] = a;
+      }
+      this.b[i] = b;
+    }
+  }
+  solve() {
+    const n = this.a.length;
+    for (let k = 0; k < n - 1; k++) {
+      for (let i = k + 1; i < n; i++) {
+        const s = this.a[i][k] / this.a[k][k];
+        for (let j = k + 1; j < n; j++) {
+          this.a[i][j] -= s * this.a[k][j];
+        }
+        this.b[i] -= s * this.b[k];
+      }
+    }
+    for (let i = n - 1; i >= 0; i--) {
+      for (let j = i + 1; j < n; j++) {
+        this.b[i] -= this.a[i][j] * this.b[j];
+      }
+      this.b[i] /= this.a[i][i];
+    }
+  }
+  print() {
+    for (const i in this.b) {
+      console.log(`x[${i}] = ${this.b[i].toExponential(10)}`);
+    }
+  }
+}
+
+const gauss = new Gauss(1000);
+gauss.set(1, 1);
+const start = performance.now();
+gauss.solve();
+const stop = performance.now();
+gauss.print();
+console.log(`${(0.001 * (stop - start)).toFixed(6)}sec`);
---
+++ gauss.py
@@ -0,0 +1,43 @@
+﻿"""
+$ python gauss.py
+"""
+import numpy as np
+import time
+
+class Gauss:
+  #n = np.int64(1000)
+  #a = np.zeros((n, n), dtype=np.float64)
+  #b = np.zeros(n, dtype=np.float64)
+  def __init__(self):
+    Gauss.n = 1000
+    self.a = np.zeros((Gauss.n, Gauss.n))
+    self.b = np.zeros(Gauss.n)
+  def set(self, a, b):
+    for i in range(Gauss.n):
+      for j in range(i, Gauss.n):
+        self.a[i][j] = a
+      self.b[i] = b
+    #self.a = [[1, 1, 1, 1, 1], [0, 1, 1, 1, 1], [0, 0, 1, 1, 1], [0, 0, 0, 1, 1], [0, 0, 0, 0, 1]]
+    #self.b = [1, 1, 1, 1, 1]
+  def solve(self):
+    for k in range(Gauss.n - 1):
+      for i in range(k + 1, Gauss.n):
+        s = self.a[i][k] / self.a[k][k]
+        for j in range(k + 1, Gauss.n):
+          self.a[i][j] -= s * self.a[k][j]
+        self.b[i] -= s * self.b[k]
+    for i in range(Gauss.n - 1, - 1, - 1):
+      for j in range(i + 1, Gauss.n):
+        self.b[i] -= self.a[i][j] * self.b[j]
+      self.b[i] /= self.a[i][i]
+  def print(self):
+    for i in range(len(self.b)):
+      print(f"x[{i}] = {self.b[i]:.10E}")
+
+gauss = Gauss()
+gauss.set(1, 1)
+start = time.time()
+gauss.solve()
+stop = time.time()
+gauss.print()
+print(f"{(stop - start):.6f}sec")
